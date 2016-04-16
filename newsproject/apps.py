b68d109457cba30734228f32e7d3afa7940bd80c
Shane O Connell <oconnes8@gmail.com>
2016-04-16 15:08:57 +0100

    initial commit

---
+++ newsproject/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class NewsprojectConfig(AppConfig):
+    name = 'newsproject'
---
+++ newsproject/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.articles_list, name='articles_list'),
+    url(r'^feeds/new', views.new_feed, name='new_feed'),
+    url(r'^feeds/', views.feeds_list, name='feeds_list'),
+]
